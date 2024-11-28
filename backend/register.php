37428bfdc5496de5c83948d24df895164ab2980a
Yusronaritama <if23.yusronaritama@mhs.ubpkarawang.ac.id>
2024-11-28 19:34:13 +0700

    Update register.php

--- backend/register.php
+++ backend/register.php
@@ -1,29 +1,44 @@
 <?php
 
+session_start();
+
 require './../config/db.php';
 
 if(isset($_POST['submit'])) {
 
+    global $db_connect;
+
+    $name = $_POST['name'];
     $email = $_POST['email'];
     $password = $_POST['password'];
+    $confirm = $_POST['confirm'];
 
-    $user = mysqli_query($db_connect,"SELECT * FROM users WHERE email = '$email'");
-    if(mysqli_num_rows($user) > 0) {
-        $data = mysqli_fetch_assoc($user);
-        
-        if(password_verify($password,$data['password'])) {
-            echo "selamat datang ".$data['name'];
-            die;
-
-            //otorisasi
-        } else {
-            echo "password salah";
-            die;
-        }
-
-    } else {
-        echo "email atau password salah";
+    if($confirm != $password) {
+        echo "password tidak sesuai dengan konfirmasi password";
         die;
     }
+
+    $usedEmail = mysqli_query($db_connect,"SELECT email FROM users WHERE email = '$email'");
+    if(mysqli_num_rows( $usedEmail) > 0) {
+        echo "email sudah digunakan";
+        die;
+    }
+
+    $password = password_hash($password,PASSWORD_DEFAULT);
+    $created_at = date('Y-m-d H:i:s',time());
+        
+    $users = mysqli_query($db_connect,"INSERT INTO users (name,email, password,created_at) VALUES
+                            ('$name','$email','$password','$created_at')");
+
+    $getUserdata = mysqli_query($db_connect, "SELECT name, role FROM users WHERE email = '$email'");
+    
+    $sessionData = mysqli_fetch_assoc($getUserdata);
+
+    $_SESSION['name'] = $sessionData['name'];
+    $_SESSION['role'] = $sessionData['role'];
+
+    header('Location:./../profile.php');
+
+
+    //echo "registrasi berhasil";
 }
-?>
